49b6fa17b8e2037145958cf6913a9856acd1e571
Angel Newton <xanewton@gmail.com>
2017-10-31 16:24:40 -0400

    Add Outlets and Action to change color

--- ColorMakerWithSliders/ViewController.swift
+++ ColorMakerWithSliders/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Outlets
+    @IBOutlet weak var colorView    : UIView!
+    @IBOutlet weak var redControl   : UISlider!
+    @IBOutlet weak var greenControl : UISlider!
+    @IBOutlet weak var blueControl  : UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set colorView on launch
+        changeColorComponent()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: Actions
+    @IBAction func changeColorComponent() {
+        
+        // Make sure the program doesn't crash if the controls aren't connected
+        if self.redControl == nil {
+            return
+        }
+        
+        //let r: CGFloat = self.redControl.isOn ? 1 : 0
+        let r: CGFloat = CGFloat(self.redControl.value)
+        let g: CGFloat = CGFloat(self.greenControl.value)
+        let b: CGFloat = CGFloat(self.blueControl.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
 
 }
 
